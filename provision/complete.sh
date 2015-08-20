eaf345a78a879a84c523f548be59d7c75c8a8286
Nick Nooney <nooney@stolaf.edu>
2015-08-19 19:35:57 -0500

    Added more information in completion message and updated .gitignore to exclude www/

--- provision/complete.sh
+++ provision/complete.sh
@@ -3,5 +3,9 @@
 # Display a completed message to the user.
 echo "=============================================================="
 echo "The Virtual Machine has been set up! You can visit the website"
-echo "at http://oleville.local/                                     "
+echo "at                  http://oleville.local/                    "
+echo "                                                              "
+echo "Note that none of the plugins that are used on the live server"
+echo "are present on this server, and they will need to be installed"
+echo "manually in the www/wp-content/plugins folder.                "
 echo "=============================================================="
